c35b03a5f1cd6b9f01e49be4f7d9d1e45930c229
noudemonts1 <36259968+noudemonts1@users.noreply.github.com>
2019-01-03 21:25:28 +0100

    Combine binary matrix with A1 and A2

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,32 @@ public class Main {
 		reader.readInput();
 	}
 	
-	public int ifAndIf(int a, int b) {
-		return a == b ? 1 : 0;
+	public int [][] numberCorrectMatrix(int [][] matrix, int [][] binaryList, int numberOfQuestions) {
+		int [][] correctMatrix = new int [(int) Math.pow(2, (double) numberOfQuestions)][matrix.length];
+		for(int i = 0; i < binaryList.length; i++) {
+			for(int j = 0; j < matrix.length; j++) {
+				int correctAnswers = countCorrectAnswers(binaryList[i], matrix[j]);
+				correctMatrix[i][j] = correctAnswers;
+				correctMatrix[correctMatrix.length-i-1][j] = numberOfQuestions-correctAnswers;
+			}
+		}
+		return correctMatrix;
+	}
+
+	/**
+	 * Applies elementwise ifandonlyif of 2 vectors, then returns the sum of the resulting vector
+	 * @param a vector a
+	 * @param b vector b
+	 * @return int
+	 */
+	public int countCorrectAnswers(int[] a, int[] b) {
+		int correctAnswers = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == b[i]) {
+				correctAnswers++;
+			}
+		}
+		return correctAnswers;
 	}
 
 }
--- src/Reader.java
+++ src/Reader.java
@@ -4,9 +4,9 @@ public class Reader {
 
 	private Scanner scanner;
 	private int n;
-	private int [][] A1;
-	private int [][] A2;
-	private int [] b;
+	private int[][] A1;
+	private int[][] A2;
+	private int[] b;
 	private String output;
 
 	public Reader(Scanner scanner) {
@@ -16,22 +16,21 @@ public class Reader {
 	public void readInput() {
 		n = scanner.nextInt();
 		int m = scanner.nextInt();
-		int m1 = m/2;
-		int m2 = m-m1;
+		int m1 = m / 2;
+		int m2 = m - m1;
 
-		A1 = new int [n][m1];
-		A2 = new int [n][m2];
-		b = new int [n];
+		A1 = new int[n][m1];
+		A2 = new int[n][m2];
+		b = new int[n];
 
-		//Fill matrices
-		for(int i = 0; i < n; i++) {
+		// Fill matrices
+		for (int i = 0; i < n; i++) {
 			String answers = scanner.next();
-			for(int j = 0; j < m; j++) {
+			for (int j = 0; j < m; j++) {
 				if (j < m1) {
 					A1[i][j] = answers.charAt(j) - 48;
-				}
-				else {
-					A2[i][j-m1] = answers.charAt(j) - 48;
+				} else {
+					A2[i][j - m1] = answers.charAt(j) - 48;
 				}
 			}
 			b[i] = scanner.nextInt();
